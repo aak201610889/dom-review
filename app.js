09e85e4b977fb8d83637d0ef756dbd5832cbc78c
ayham-kattan <aak201610885@gmail.com>
2025-05-03 05:04:56 -0700

    add -attrubite and classes

--- app.js
+++ app.js
@@ -8,50 +8,69 @@
 
 // --------------------------------------------------------------
 
-const title=document.getElementById('main-heading')
-console.log(title)
-const listItem=document.getElementsByClassName('list-items')
-console.log(listItem)
-const container=document.querySelector('div')
-console.log(container)
-const alldives=document.querySelectorAll('div')
-console.log(alldives)
+const title = document.getElementById("main-heading");
+console.log(title);
+const listItem = document.getElementsByClassName("list-items");
+console.log(listItem);
+const container = document.querySelector("div");
+console.log(container);
+const alldives = document.querySelectorAll("div");
+console.log(alldives);
 
 // --------------------------------
 // you can use slectore for calling classes or ids
 
-const titleSelector=document.querySelector("#main-heading")
+const titleSelector = document.querySelector("#main-heading");
 
-console.log(titleSelector)
-titleSelector.style.color='red';
+console.log(titleSelector);
+titleSelector.style.color = "red";
 
-const containreSelector=document.querySelector(".container")
+const containreSelector = document.querySelector(".container");
 
+containreSelector.style.width = "60%";
 
-containreSelector.style.width='60%';
+const listItemsSelector = document.querySelectorAll(".list-items");
 
-
-const listItemsSelector=document.querySelectorAll(".list-items")
-
-console.log(listItemsSelector)
+console.log(listItemsSelector);
 // listItemsSelector.style.fontSize='2rem ' ==>not work because is array it's need for
 
 for (const element of listItemsSelector) {
-  element.style.color='blue'
-  element.style.fontSize='1.3rem'
+  element.style.color = "blue";
+  element.style.fontSize = "1.3rem";
 }
 
 // ------------------ handling element
 //create
-const ul=document.querySelector('ul');
-const li=document.createElement('li');
-ul.append(li)
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+ul.append(li);
 //update
 
-const firstItem=document.querySelector('.list-items')
-console.log(firstItem.innerText)
-console.log(firstItem.innerHTML)
-console.log(firstItem.textContent)
+const firstItem = document.querySelector(".list-items");
+console.log(firstItem.innerText);
+console.log(firstItem.innerHTML);
+console.log(firstItem.textContent);
+
+firstItem.innerText = "Xmen";
+
+// delete
+
+li.remove()
 
 
-firstItem.innerText="Xmen"
+// -------------------------------- modify Attribute & classes
+
+const liSelector = document.querySelector("li");
+liSelector.innerText = "X-men";
+
+          // ---for id -----
+
+liSelector.setAttribute("id", "main-heading");
+console.log(liSelector.getAttribute('id'));
+liSelector.removeAttribute("id");
+
+          // ---for class -----
+
+liSelector.classList.add('list-items')
+console.log(liSelector.classList.contains('list-items'));
+liSelector.classList.remove("list-items");
