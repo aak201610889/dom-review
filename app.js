45850dd66829130f95cc5df8c53e6475ba51e82f
ayham-kattan <aak201610885@gmail.com>
2025-05-03 04:21:07 -0700

    logs-all-doms

--- app.js
+++ app.js
@@ -3,7 +3,17 @@
 // GetElementById()
 // GetElementByClassName()
 // GetElementByTagName()
-// querySelector()
+// querySelector()=====>select the first slector (div)
 // querySelectorAll()
 
 // --------------------------------------------------------------
+
+const title=document.getElementById('main-heading')
+console.log(title)
+const listItem=document.getElementsByClassName('list-items')
+console.log(listItem)
+const container=document.querySelector('div')
+console.log(container)
+const alldives=document.querySelectorAll('div')
+console.log(alldives)
+
