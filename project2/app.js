e3ef5d14d8cd0ff144c74fc67c0edc7072211072
ayham-kattan <aak201610885@gmail.com>
2025-05-05 12:02:40 -0700

    finish

--- project2/app.js
+++ project2/app.js
@@ -1,14 +1,13 @@
-const btn = document.querySelector('.btn');
-const modal = document.querySelector('.modal');
+const btn = document.querySelector(".btn");
+const modal = document.querySelector(".modal");
 const closeBtn = modal.firstElementChild;
 
-btn.addEventListener('click', () => {
-  modal.classList.add('show');
-  btn.classList.add('hidden');
-
+btn.addEventListener("click", () => {
+  modal.classList.add("show");
+  btn.classList.add("hidden");
 });
 
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('show');
-  btn.classList.remove('hidden');
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+  btn.classList.remove("hidden");
 });
